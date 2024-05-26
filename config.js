deb19ce99107382f96714db2b753273c557d4d34
angulo1202 <angulo_ramon@hotmail.com>
2024-05-26 18:31:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbzFOPmcifX4KC-QX1MVi9vSE3RkeN8Ye6NO4pr7u1KZ-Ai3qNqK3OrHRZxZ_4qPGmmS/exec";
